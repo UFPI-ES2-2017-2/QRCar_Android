5c3815eadce38e9215445c16b64cb2d59c1ba464
coliveira <coliveiraluca@gmail.com>
2017-11-12 01:09:35 -0300

    Prototype of the facade implemented, and ready to create new methods.

--- app/src/main/java/es/ufpi/br/qrcar/facade/Facade.java
+++ app/src/main/java/es/ufpi/br/qrcar/facade/Facade.java
@@ -1,4 +1,26 @@
 package es.ufpi.br.qrcar.facade;
 
+import es.ufpi.br.qrcar.control.QRCodeController;
+import es.ufpi.br.qrcar.control.ContractController;
+import es.ufpi.br.qrcar.control.VehiclesController;
+import es.ufpi.br.qrcar.control.ClientsController;
+import es.ufpi.br.qrcar.control.UsersController;
+
 public class Facade {
+    private QRCodeController qrCodeController;
+    private ContractController contractController;
+    private VehiclesController vehiclesController;
+    private ClientsController clientsController;
+    private UsersController usersController;
+
+    public Facade()
+    {
+        this.qrCodeController = new QRCodeController();
+        this.contractController = new ContractController();
+        this.vehiclesController = new VehiclesController();
+        this.clientsController = new ClientsController();
+        this.usersController = new UsersController();
+    }
+
+
 }
