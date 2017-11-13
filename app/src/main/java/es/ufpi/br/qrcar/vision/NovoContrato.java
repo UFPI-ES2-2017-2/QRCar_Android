3625e8dd00c2712884b6f89cc11297a7fbb414e1
Jonathas Evangelista <joninhaevang@gmail.com>
2017-11-13 14:41:18 -0300

    Criando a ação do criar

--- app/src/main/java/es/ufpi/br/qrcar/vision/NovoContrato.java
+++ app/src/main/java/es/ufpi/br/qrcar/vision/NovoContrato.java
@@ -2,9 +2,13 @@ package es.ufpi.br.qrcar.vision;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import es.ufpi.br.qrcar.R;
+import es.ufpi.br.qrcar.entity.Contract;
+import es.ufpi.br.qrcar.facade.Facade;
 
 /**
  * Created by root on 12-11-2017.
@@ -16,7 +20,8 @@ public class NovoContrato extends AppCompatActivity{
     private EditText price;
     private EditText client_cpf;
     private EditText plate_id;
-
+    private Button criar;
+    private Facade facade;
     @Override
     protected void onCreate(Bundle savedInstance)
     {
@@ -27,5 +32,11 @@ public class NovoContrato extends AppCompatActivity{
         this.price = (EditText)findViewById(R.id.novo_contrato_preco_input);
         this.client_cpf = (EditText)findViewById(R.id.novo_contrato_cpf_input);
         this.plate_id = (EditText)findViewById(R.id.novo_contrato_placa_input);
+        this.criar = (Button)findViewById(R.id.novo_contrato_criar_button);
+        facade = (Facade) getIntent().getSerializableExtra("Facade");
+        this.criar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {}
+        });
     }
 }
