1779dcda147fe503e947cf4d394e43738d6d1127
Jonathas Evangelista <joninhaevang@gmail.com>
2017-11-13 14:41:32 -0300

    Criando o a classe Sobre

---
+++ app/src/main/java/es/ufpi/br/qrcar/vision/Sobre.java
@@ -0,0 +1,18 @@
+package es.ufpi.br.qrcar.vision;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import es.ufpi.br.qrcar.R;
+
+/**
+ * Created by jonin on 13/11/2017.
+ */
+
+public class Sobre extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+        setContentView(R.layout.activity_sobre);
+    }
+}
